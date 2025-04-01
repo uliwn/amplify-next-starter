c33728461c217fc5d3decc093ad0261809d6ce30
Pauli Winkelmann <pauli.winkelmann@ti8m.ch>
2025-04-01 16:07:20 +0200

    Add Authenticator

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
+import { Authenticator } from '@aws-amplify/ui-react';
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
@@ -32,21 +33,27 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+      <Authenticator>
+          {({ signOut, user }) => (
+              <main>
+                  <h1>My todos</h1>
+                  <h1>Hello {user?.username}</h1>
+                  <button onClick={signOut}>Sign out</button>
+                  <button onClick={createTodo}>+ new</button>
+                  <ul>
+                      {todos.map((todo) => (
+                          <li key={todo.id}>{todo.content}</li>
+                      ))}
+                  </ul>
+                  <div>
+                      🥳 App successfully hosted. Try creating a new todo.
+                      <br/>
+                      <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+                          Review next steps of this tutorial.
+                      </a>
+                  </div>
+              </main>
+          )}
+      </Authenticator>
   );
 }
